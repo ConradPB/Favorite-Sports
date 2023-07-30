3b4a5c2c31094ed0076e7664f17a9293093d8642
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-07-30 16:22:30 +0300

    updated all sports slice file

---
+++ sports/src/features/allSports/allSportsSlice.js
@@ -0,0 +1,21 @@
+import allSportsData from '../data';
+
+export const loadData = () => {
+    return {
+      type: 'allSports/loadData',
+      payload: allSportsData
+    }
+  };
+  
+  
+  
+const initialAllSports = [];
+export const allSportsReducer = 
+  (allSports = initialAllSports, action) => {
+    switch (action.type) {
+      case 'allSports/loadData':
+        return action.payload
+          default: return allSports;
+  
+    }
+  }
