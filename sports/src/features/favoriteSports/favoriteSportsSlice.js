9a2fe3debb4d37d9ffbab13a329948353d4d093f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-07-30 15:42:07 +0300

    updated favorite sports slice file

---
+++ sports/src/features/favoriteSports/favoriteSportsSlice.js
@@ -0,0 +1,32 @@
+export const addSport = (sport) => {
+    return {
+      type: 'favoriteSports/addSport',
+      payload: sport
+    }
+  };
+  
+export const removeSport = (sport) => {
+    return {
+      type: 'favoriteSports/removeSport',
+      payload: sport
+    }
+  }
+
+
+  const initialFavoriteSports = [];
+  const favoriteSportsReducer = 
+  (favoriteSports = initialFavoriteSports, action) => {
+    switch (action.type) {
+      case 'favoriteSports/addSport':
+        return [...favoriteSports, 
+          action.payload];
+  
+      case 'favoriteSports/removeSport':
+        return [
+          ...favoriteSports.filter(e => e.id !== action.payload.id)
+        ]
+  
+          default: return favoriteSports
+  
+    }
+  }
