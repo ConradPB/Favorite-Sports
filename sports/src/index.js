ae90eaf15fd68cbc21076ba30e8c10c938d4d8dd
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-07-31 04:09:55 +0300

    updated app/updated index.js

--- sports/src/index.js
+++ sports/src/index.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { store } from './app/store.js'
+import { App } from './app/App.js';
+// Import 'store' here.
 
-const container = document.getElementById('root');
-const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+const render = () => {
+  // Pass `state` and `dispatch` props to <App />
+  ReactDOM.render(
+    <App 
+    state={store.getState()}
+    dispatch={store.dispatch}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    />,
+    document.getElementById('root')
+  )
+}
+store.subscribe(render);
