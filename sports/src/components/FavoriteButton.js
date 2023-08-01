1af9eb708a906b05665e094c21a553967f2be777
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-01 04:14:42 +0300

    updated app/updated FavoriteButton.js

--- sports/src/components/FavoriteButton.js
+++ sports/src/components/FavoriteButton.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function FavoriteButton({ children, onClickHandler, icon }) {
   return (
-    <button className="favorite-button" onClick={onClickHandler}>
-      <img className="heart-icon" alt="" src={icon} />
+    <button className='favorite-button' onClick={onClickHandler}>
+      <img className='heart-icon' alt='' src={icon} />
       {children}
     </button>
   );
