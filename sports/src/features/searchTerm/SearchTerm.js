9003abe45a6f9029047d0664da267dbea60abd1f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-04 04:04:48 +0300

    updated app/updated searchTerm.js

---
+++ sports/src/features/searchTerm/SearchTerm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const searchIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/search.svg'
+const clearIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/clear.svg'
+
+import { setSearchTerm, clearSearchTerm } from './searchTermSlice.js';
+
+export const SearchTerm = (props) => {
+  
+  const { searchTerm, dispatch } = props;
+
+  const onSearchTermChangeHandler = (e) => {
+    const userInput = e.target.value;
+    dispatch(setSearchTerm(userInput));
+  };
+  
+  const onClearSearchTermHandler = () => {
+    dispatch(clearSearchTerm());
+  };
