d7b6c0007e5d155e604dc85b7634f123f3374112
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-05 03:12:32 +0300

    updated app/updated index.js. Added Provider Component

--- sports/src/index.js
+++ sports/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app/App.js';
 import { store } from './app/store.js';
+import { Provider } from '@reduxjs/toolkit';
 
 
 const render = () => {
   // Pass `state` to `dispatch` props to <App />
   ReactDOM.render(
-    <App 
-    state={store.getState()}
-    dispatch={store.dispatch}
-    />,
+    <Provider store={store} >
+    <App />
+    </Provider>,
     document.getElementById('root')
   )
 }
