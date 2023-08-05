a0adb3cc928643d7719d6e83d0d78afed22e6e9c
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-06 00:39:58 +0300

    updated app/updated allSportsSlice.js

--- sports/src/features/allSports/allSportsSlice.js
+++ sports/src/features/allSports/allSportsSlice.js
@@ -1,21 +1,25 @@
 import allSportsData from "../../data";
+import { selectSearchTerm } from '../searchTerm/searchTermSlice';
 
 export const loadData = () => {
-    return {
-      type: 'allSports/loadData',
-      payload: allSportsData
-    }
-  };
-  
-  
-  
-const initialAllSports = [];
-export const allSportsReducer = 
-  (allSports = initialAllSports, action) => {
-    switch (action.type) {
-      case 'allSports/loadData':
-        return action.payload
-          default: return allSports;
-  
-    }
+  return {
+    type: 'allSports/loadData',
+    payload: allSportsData
   }
+}
+
+const initialState = [];
+export const allSportsReducer = (allSports = initialState, action) => {
+  switch (action.type) {
+    case 'allSports/loadData':
+      return action.payload;
+    case 'favoriteSports/addSport':
+      return allSports.filter(sport => sport.id !== action.payload.id);
+    case 'favoriteSports/removeSport':
+      return [...allSports, action.payload]
+    default:
+      return allSports;
+  }
+}
+
+
