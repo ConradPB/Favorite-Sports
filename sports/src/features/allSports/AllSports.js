da2b4bb40591803b1bcc11ed283ad140c8fffc4c
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-06 22:46:23 +0300

    updated app/updated allSports.js

--- sports/src/features/allSports/AllSports.js
+++ sports/src/features/allSports/AllSports.js
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { addSport } from '../favoriteSports/favoriteSportsSlice';
 import { loadData, selectFilteredAllSports } from './allSportsSlice';
-import FavoriteButton from "../../components/FavoriteButton";
-import Sport from "../../components/Sport";
+import FavoriteButton from '../../components/FavoriteButton';
+import Sport from '../../components/Sport';
 const favoriteIconURL = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/favorite.svg';
 
 export const AllSports = () => {
@@ -23,4 +23,20 @@ export const AllSports = () => {
     dispatch(addSport(sport));
   };
 
-}
+  return (
+    <div className='sports-container'>
+      {allSports.map((sport) => (
+        <Sport sport={sport} key={sport.id}>
+          <FavoriteButton
+            onClickHandler={() => onAddSportHandler(sport)}
+            icon={favoriteIconURL}
+          >
+            Add to Favorites
+          </FavoriteButton>
+        </Sport>
+      ))}
+    </div>
+  );
+};
+
+
