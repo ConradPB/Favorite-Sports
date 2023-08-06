f6421ca0034a197561cacf5597c9ec56475c045f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-06 23:29:21 +0300

    updated app/updated FavoriteSports

--- sports/src/features/favoriteSports/FavoriteSports.js
+++ sports/src/features/favoriteSports/FavoriteSports.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { removeSport, selectFilteredFavoriteSports } from './favoriteSportsSlice.js';
-import FavoriteButton from "../../components/FavoriteButton.jsn";
-import Sport from "../../components/Sport.js";
+import FavoriteButton from '../../components/FavoriteButton.jsn';
+import Sport from '../../components/Sport.js';
 const unfavoriteIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/unfavorite.svg';
 
-export const FavoriteRecipes = () =>{
+export const FavoriteSports = () =>{
   const favoriteSports = useSelector(selectFilteredFavoriteSports);
   const dispatch = useDispatch();
 
@@ -14,4 +14,23 @@ export const FavoriteRecipes = () =>{
     // Dispatch the action below.
     dispatch(removeSport(sport))
   };
-}
+  return (
+    <div className='sports-container'>
+      {favoriteSports.map(createSportComponent)}
+    </div>
+  );
+
+  // Helper Function
+  function createSportComponent(sport) {
+    return (
+      <Sport sport={sport} key={sport.id}>
+        <FavoriteButton
+          onClickHandler={() => onRemoveSportHandler(sport)}
+          icon={unfavoriteIconUrl}
+        >
+          Remove Favorite
+        </FavoriteButton>
+      </Sport>
+    )
+  } 
+};
