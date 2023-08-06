00d28d1607e3e97700fc53ce302bcf83701bcd46
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-06 03:15:23 +0300

    updated app/updated allSports.js

--- sports/src/features/allSports/AllSports.js
+++ sports/src/features/allSports/AllSports.js
@@ -1,39 +1,24 @@
-import { addSport } from '../favoriteSports/favoriteSportsSlice';
-import { loadData } from './allSportsSlice.js'
 
 import React, { useEffect } from 'react';
+// Implement the import statements below.
+import { useSelector } from '@reduxjs/toolkit';
+import { loadData, selectFilteredAllSports } from './allSportsSlice';
+import { addSport } from '../favoriteSports/favoriteSportsSlice';
 import FavoriteButton from "../../components/FavoriteButton";
-import Sport from "../../components/Sport";
+import Recipe from "../../components/Sport";
+const favoriteIconURL = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/favorite.svg';
 
-const favoriteIconURL = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/favorite.svg'
-
-export const AllSports = (props) => {
+export const AllSports = () => {
   
-  const { allSports, dispatch } = props;
-
+  const allSports = useSelector(selectFilteredAllSports)
+  
   const onFirstRender = () => {
-    dispatch(loadData());
-  }
-  useEffect(onFirstRender, [dispatch])
   
+  }
+  useEffect(onFirstRender, []);
+
   const onAddSportHandler = (sport) => {
-    dispatch(addSport(sport));
+   
   };
 
-  return (
-    <div className='sports-container'>
-      {allSports.map((sport) => (
-        <Sport sport={sport} key={sport.id}>
-          <FavoriteButton
-            onClickHandler={() => onAddSportHandler(sport)}
-            icon={favoriteIconURL}
-          >
-            Add to Favorites
-          </FavoriteButton>
-        </Sport>
-      ))}
-    </div>
-  );
-};
-
-
+}
