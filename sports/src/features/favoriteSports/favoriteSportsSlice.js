d0f9c5abf17f93d71134ad96ac4a6cf809828ca3
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-08 18:15:33 +0300

    updated app/updated favoriteSportsSlice.js

--- sports/src/features/favoriteSports/favoriteSportsSlice.js
+++ sports/src/features/favoriteSports/favoriteSportsSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { selectSearchTerm } from '../searchTerm/searchTermSlice.js';
 
 export const favoriteSportsSlice = createSlice({
-  name: "favoriteSports",
+  name: 'favoriteSports',
   initialState: [],
   reducers: {
     addSport: (state, action) => {
@@ -13,3 +13,28 @@ export const favoriteSportsSlice = createSlice({
     },
   },
 });
+
+export const selectFavoriteSports = (state) => state.favoriteSports;
+
+export const selectFilteredFavoriteSports = (state) => {
+  const favoriteSports = selectFavoriteSports(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return favoriteSports.filter((sport) =>
+    sport.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+};
+
+console.log(favoriteSportsSlice.name)
+
+for (const action in favoriteSportsSlice.actions) {
+  console.log(action)
+}
+
+export const {
+  addSport,
+  removeSport,
+} = favoriteSportsSlice.actions;
+
+console.log(favoriteSportsSlice);
+export default favoriteSportsSlice.reducer
