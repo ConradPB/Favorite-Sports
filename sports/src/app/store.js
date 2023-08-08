0e5ac445928473e84b44c8c58689b1076fb7bae3
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-08 23:24:27 +0300

    updated app/updated store.js

--- sports/src/app/store.js
+++ sports/src/app/store.js
@@ -1,22 +1,11 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import favoriteSportsReducer from '../features/favoriteSports/favoriteSportsSlice';
+import searchTermReducer from '../features/searchTerm/searchTermSlice.js';
+import allSportsReducer from '../features/allSports/allSportsSlice';
 
-import { favoriteSportsReducer } from '../features/favoriteSports/favoriteSportsSlice'
-import { searchTermReducer } from '../features/searchTerm/searchTermSlice'
-import { allSportsReducer } from '../features//allSports/allSportsSlice'
-
-
-
-
-const reducers = {
-  allSports: allSportsReducer,
+export default configureStore({
+  reducer: {
+  favoriteSports: favoriteSportsReducer,
   searchTerm: searchTermReducer,
-  favoriteSports: favoriteSportsReducer 
-}
-
-const rootReducer = combineReducers(reducers);
-
-
-export const store = configureStore({
-  reducer: rootReducer
+  allSports: allSportsReducer}
 });
-
