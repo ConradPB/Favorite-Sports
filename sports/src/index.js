44098c63a49e8f3b51137916f787577f69031f24
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-09 17:06:14 +0300

    updated app/updated index.js

--- sports/src/index.js
+++ sports/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app/App.js';
-import { store } from './app/store.js';
-import { Provider } from '@reduxjs/toolkit';
+import store  from '../src/app/store.js';
+import { Provider } from 'react-redux';
 
 
 const render = () => {
