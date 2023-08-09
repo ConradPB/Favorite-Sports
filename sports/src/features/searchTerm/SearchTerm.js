8757c9e9a6a718a574588fa050e9fcb84591134f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-09 06:33:44 +0300

    updated app/updated SearchTerm.js

--- sports/src/features/searchTerm/SearchTerm.js
+++ sports/src/features/searchTerm/SearchTerm.js
@@ -19,4 +19,26 @@ export const SearchTerm = () => {
   const onClearSearchTermHandler = () => {
     dispatch(clearSearchTerm());
   };
-}
+
+  return (
+    <div id='search-container'>
+      <img id='search-icon' alt='' src={searchIconUrl} />
+      <input
+        id='search'
+        type='text'
+        value={searchTerm}
+        onChange={onSearchTermChangeHandler}
+        placeholder='Search sports'
+      />
+      {searchTerm.length > 0 && (
+        <button
+          onClick={onClearSearchTermHandler}
+          type='button'
+          id='search-clear-button'
+        >
+          <img src={clearIconUrl} alt='' />
+        </button>
+      )}
+    </div>
+  );
+};
