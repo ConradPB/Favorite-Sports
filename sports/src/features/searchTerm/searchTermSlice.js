d566729f2772205239f27c904731c5916bdc1c1e
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-09 17:14:18 +0300

    updated app/updated searchTermSlice.js

--- sports/src/features/searchTerm/searchTermSlice.js
+++ sports/src/features/searchTerm/searchTermSlice.js
@@ -1,43 +1,19 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+const initialState = ''
 
-import { setSearchTerm, clearSearchTerm, selectSearchTerm } from './searchTermSlice.js';
-const searchIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/search.svg';
-const clearIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/clear.svg';
+const searchTermReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'searchTerm/setSearchTerm':
+      return action.payload;
+    case 'searchTerm/clearSearchTerm':
+      return '';
+    default:
+      return state;
+  }
+}
 
-
-export const SearchTerm = () => {
-  const searchTerm = useSelector(selectSearchTerm);
-  const dispatch = useDispatch();
-
-  const onSearchTermChangeHandler = (e) => {
-    const userInput = e.target.value;
-    dispatch(setSearchTerm(userInput));
-  };
-  
-  const onClearSearchTermHandler = () => {
-    dispatch(clearSearchTerm());
-  };
-
-  return (
-    <div id='search-container'>
-      <img id='search-icon' alt='' src={searchIconUrl} />
-      <input
-        id='search'
-        type='text'
-        value={searchTerm}
-        onChange={onSearchTermChangeHandler}
-        placeholder='Search sports'
-      />
-      {searchTerm.length > 0 && (
-        <button
-          onClick={onClearSearchTermHandler}
-          type='button'
-          id='search-clear-button'
-        >
-          <img src={clearIconUrl} alt='' />
-        </button>
-      )}
-    </div>
-  );
-};
+export function setSearchTerm(term) {
+  return {
+    type: 'searchTerm/setSearchTerm',
+    payload: term
+  }
+}
